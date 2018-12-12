d8cc371bb35b4ff7bbfe03067aa9d2920ada8bb8
Roy <gsson@namyang-intl.com>
2018-12-12 22:24:26 +0900

    - 2018년12월11일 LinearLayout 및 버튼, 텍스트뷰 작동 테스트

---
+++ practice/src/main/java/com/example/practice/ActivityLab2_LinearLayout.java
@@ -0,0 +1,19 @@
+package com.example.practice;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ActivityLab2_LinearLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab2__linear_layout);
+
+    }
+
+}
